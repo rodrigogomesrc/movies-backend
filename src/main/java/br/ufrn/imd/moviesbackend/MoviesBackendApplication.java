b17243ddf15b8c4bcaebe7ddcdec0b86d13eed95
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2022-07-16 17:11:28 -0300

    create project

---
+++ src/main/java/br/ufrn/imd/moviesbackend/MoviesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.moviesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/moviesbackend/MoviesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.moviesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
